85f6f7459d14569a348a045eb16b9a340a3749cb
Itai Barkav <itaibarkav@gmail.com>
2021-02-22 20:01:37 +0200

    Update server

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/daily'));
+app.use(express.static('./dist/manage-tool'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', { root: 'dist/daily/' })
+  res.sendFile('index.html', { root: 'dist/manage-tool/' })
 );
 
 // Start the app by listening on the default Heroku port
